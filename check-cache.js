1067db0888db64dc10098b8efabfdc4b3fa6acf5
Aneeq <ahmedaneeq.official@gmail.com>
2026-09-03 22:20:44 +0530

    zz

--- check-cache.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const { getProductsAction } = require('./src/app/actions/products.js') || {};
-// Just test the server action manually
-async function test() {
-  try {
-    const fetch = global.fetch; // just to test
-  } catch(e) {}
-}
